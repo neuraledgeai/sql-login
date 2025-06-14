cb759c07528d18456005a4350b08e45f89fe48c1
Anoop Kumar U <anooprockstar10@gmail.com>
2025-06-14 17:21:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import re
 import datetime
-import firebase_admin
+import firebase_admin 
 from firebase_admin import credentials, firestore
 
 # ---------- Firebase Setup ----------
